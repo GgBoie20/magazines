ae20d7689bc8fb2f66671837716698d494d32077
GAMMER <ladkegitesh20@gmail.com>
2024-01-12 01:45:52 +0530

    commit

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,21 +2,24 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
+import Jan from './front.svg'
+import Dec from './Dec.png'
+import Oct from './Oct.png'
 
 const DemoPaper = styled(Paper)(({ theme }) => ({
-  width: 300,
-  height: 300,
-  padding: theme.spacing(2),
+  width: 260,
+  height: 350,
+  padding: theme.spacing(1),
   ...theme.typography.body2,
   textAlign: 'center',
 }));
 
+const FrontImage=[Jan, Dec, Oct]
+
 export default function Layout() {
   return (
     <Stack direction="row" spacing={6}>
-      <DemoPaper square={false}>heloo</DemoPaper>
-      <DemoPaper square={false}>heloo</DemoPaper>
-      <DemoPaper square={false}>heloo</DemoPaper>
+      {FrontImage.map((image) => ( <DemoPaper key={image} square={false} component="img" src={image} object-fit= "fill"></DemoPaper>))}
     </Stack>
     
   );
